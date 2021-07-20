b4128cb7b8abf5f65ed3b4ac6b98cda73ce26bcb
GeorgeGxx <jgallagaa@gmail.com>
2021-07-19 23:05:37 -0500

    Update

---
+++ profile.js
@@ -0,0 +1,116 @@
+export const skills = [
+  {
+    skill: "JavaScript",
+    percentage: 70,
+  },
+  {
+    skill: "Python 3",
+    percentage: 60,
+  },
+  {
+    skill: "Java 8",
+    percentage: 50,
+  },
+  {
+    skill: "Linux RHEL 7",
+    percentage: 80,
+  },
+  {
+    skill: "Microsoft Azure DevOps",
+    percentage: 60,
+  },
+  {
+    skill: "Amazon AWS",
+    percentage: 50,
+  },
+  {
+    skill: "Google Cloud GCP",
+    percentage: 50,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software Developer at Independent",
+    description:
+      "Responsibilities: • Web layout, develop in Front-end and Back-end. • Domains, SEO, UML, prioritize changes, DevSecOps Pipeline. • Pre-sales and customer service working under the Scrum Agile Framework. • Achievements: Publish several startups on the internet and increase sales. • Tools > HTML5, CSS3, JS, Node.js, React 16, Angular 11, Vue 3, APIs REST, SOA, Java 8, 11, MVC, Python 3, MVT, MySQL, Postman, VSCode, Linux Bash, Git, Docker, Kubernetes, Firebase, Azure, AWS, IBMCloud",
+    from: 2015,
+  },
+  {
+    title: "Implementation Consultant II at Oracle",
+    description:
+      "Responsibilities: • Implementation of HA PMS Servers, Networks, upgrades, ITSec Tshoot. • Migration, DRP-Systems, Waterfall Framework (PMBOK v4-v5). KPIs: Request response time, hours / IT-System Downtime. • Achievements: Systems implemented without incidents, deployment of DRP with 1hr Downtime, OPERA PMSv5 Certification in Bs.As., Argentina. • Tools > WS08 HA, OracleLinux, VMwareESXi5, Symantec, Veeam BMS7, PSS, ORACLE 11g RAC, PL-SQL, DataGuard, RMAN, HP, Cisco, Barracuda Networks",
+    from: 2014,
+    to: 2015,
+  },
+  {
+    title: "IT Manager at ETS Consulting & KornFerry International",
+    description:
+      "Responsibilities: • Responsible for the IT area, service management. • Technical support, supplier service, Tshoot. • Creation of IT budgets, SLA’s, 2 Engineers in charge. • Achievements: Reduction of costs in network services, 2% of issues in networks and IS, successful refresh of Cisco ASA5516-X Firewall. • Tools > Microsoft, ADS, MacOSX, HyperV, Symantec, Citrix, CiscoRSSec, TP-Link",
+    from: 2013,
+    to: 2014,
+  },
+];
+
+export const projects = [
+  {
+    name: "Awesome Website 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.jpeg",
+  },
+  {
+    name: "Awesome Website 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio2.jpg",
+  },
+  {
+    name: "Awesome Website 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio3.png",
+  },
+  {
+    name: "Awesome Website 4",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio4.png",
+  },
+  {
+    name: "Awesome Website 5",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio5.jpeg",
+  },
+  {
+    name: "Awesome Website 6",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio6.jpeg",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
